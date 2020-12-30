9de48323669fba645452f6964482dc88916736fc
Deeton Rushton <76126281+DeetonRushy@users.noreply.github.com>
2020-12-30 22:05:18 +0000

    Comment fixes
    ^

--- src/lGui.cpp
+++ src/lGui.cpp
@@ -5,13 +5,13 @@ HWND g_Window;
 
 int main()
 {
-	// OnStart is important, it initialized the static members of the Gui class.
-	// This function allows you to optionally set the size & title.
+	// OnStart is important, it initializes the static members of the Gui class.
+	// This function allows you to optionally set the size, title & state.
 
 	Gui::OnStart();
 
 	// Creating an object of Renderer & calling Init to initialize all the needed D3D9 devices.
-	// This also creates our window, window classes, DirectX devices.
+	// This also creates our window, window classes & DirectX devices.
 
 	Renderer worker;
 	worker.Init(g_Window, L"lGuiMainWindowClass"); // change the window class name if you really want to.
